232dd69414d70cbcb1137a52d9081a9c07b097f2
Maxim Savenkov <max_savenkov@mail.ru>
2023-05-15 17:21:53 +0200

    Fixed docs

--- src/render/common3d.rs
+++ src/render/common3d.rs
@@ -3,7 +3,10 @@ use crate::render::color::{Color, RGB};
 
 pub struct Model {
     vert_count: u32,
-    data: Vec<f32>,///@see: consts.rs
+    /// see: [`consts`]
+    ///
+    /// [`consts`]: crate::render::consts
+    data: Vec<f32>,
     indices: Vec<u32>,
 }
 
@@ -11,7 +14,7 @@ pub struct Material {}
 
 pub struct Light {
     direction: Vec3,
-    position: Vec<3>,
+    position: Vec<f32>,
     attenuation: f32,
     color: Color<RGB, f32>,
 }
