c7078b818a302b79019e34f386916bb4b93e424e
Maxim Savenkov <max_savenkov@mail.ru>
2024-04-06 16:26:18 +0200

    Unknown error

--- Core/src/math/vec.rs
+++ Core/src/math/vec.rs
@@ -1,39 +1,28 @@
 use std::ops::{Deref, DerefMut, Mul, MulAssign};
-use std::simd::{f32x2, f32x4};
+use std::simd::f32x4;
 
 use mvutils::unsafe_utils::Unsafe;
 
 #[derive(Default, Debug, Copy, Clone)]
 #[repr(C)]
-pub struct Vec2(f32x2);
-
-impl Vec2 {
-    pub fn new(x: f32, y: f32) -> Self {
-        Self([x, y].into())
-    }
-
-    pub fn splat(val: f32) -> Self {
-        Self([val; 2].into())
-    }
-}
-
-#[repr(C)]
-pub struct _DerefVec2 {
+pub struct Vec2 {
     pub x: f32,
     pub y: f32,
 }
 
-impl Deref for Vec2 {
-    type Target = _DerefVec2;
-
-    fn deref(&self) -> &Self::Target {
-        unsafe { Unsafe::cast_ref(self) }
+impl Vec2 {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self {
+            x,
+            y,
+        }
     }
-}
 
-impl DerefMut for Vec2 {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { Unsafe::cast_mut(self) }
+    pub fn splat(val: f32) -> Self {
+        Self {
+            x: val,
+            y: val,
+        }
     }
 }
 
@@ -92,7 +81,7 @@ impl MulAssign<f32> for Vec4 {
 }
 
 #[repr(C)]
-pub struct _DerefVec4 {
+pub struct DerefVec4 {
     pub x: f32,
     pub y: f32,
     pub z: f32,
@@ -100,7 +89,7 @@ pub struct _DerefVec4 {
 }
 
 impl Deref for Vec4 {
-    type Target = _DerefVec4;
+    type Target = DerefVec4;
 
     fn deref(&self) -> &Self::Target {
         unsafe { Unsafe::cast_ref(self) }
--- Engine/Render2D/src/shaders/default.frag
+++ Engine/Render2D/src/shaders/default.frag
@@ -1,4 +1,4 @@
-#version 420
+#version 460
 
 layout (location = 0) out vec4 outColor;
 layout(location = 1) in vec4 inTexCoords;
