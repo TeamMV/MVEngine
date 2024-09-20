f69c40c55285d509706190431509663d7a434f33
mqxf <mqxf@mvteam.dev>
2024-09-20 14:24:07 +0200

    add macro expansion for demo

--- Engine/Ui/tests/test.rs
+++ Engine/Ui/tests/test.rs
@@ -2,7 +2,7 @@ use mvengine_ui::elements::UiElement;
 use mvengine_ui::styles::UiStyle;
 use mvengine_ui::uix::{global_state, use_state, DynamicUi, UiCompoundElement};
 use mvutils::state::State;
-use mvutils::when;
+use mvutils::{update, when};
 use uiproc::{ui, uix};
 
 fn some_function(input: i32) -> u32 {
@@ -31,5 +31,61 @@ pub fn MyComponent() -> UiElement {
             <Text>{{ format!("Clicked 2x{} times", number) }}</Text>
             <Button onclick={{ *clicks.write() += 1 }}>Click</Button>
         </Div>
+    }
+}
+
+pub struct MyComponent2 {
+    _cached: mvutils::once::CreateOnce<DynamicUi>,
+    attributes: mvengine_ui::attributes::Attributes,
+    style: UiStyle,
+    clicks: State<i32>,
+    shown: State<bool>,
+    number: State<i32>,
+    global: State<i32>,
+}
+impl UiCompoundElement for MyComponent2 {
+    fn new(attributes: Option<mvengine_ui::attributes::Attributes>, style: Option<UiStyle>) -> Self
+    where
+        Self: Sized,
+    {
+        let attributes = attributes.unwrap_or(mvengine_ui::attributes::Attributes::new());
+        let style = style.unwrap_or(UiStyle::default());
+        let clicks = State::new(0);
+        let shown = State::new(false);
+
+        let number = State::new(*clicks.read() * 2);
+
+        let global = get_global_state();
+
+        Self { attributes, style, _cached: mvutils::once::CreateOnce::new(), clicks, shown, number, global }
+    }
+
+    fn generate(&self) -> UiElement {
+        let clicks = self.clicks.clone();
+        let global = self.global.clone();
+        let shown = self.shown.clone();
+        let number = self.number.clone();
+        when!([clicks] => {
+            *number.write() = *clicks.read() * 2;
+        });
+
+        ui! {
+            <Div>
+                <Text>{{ format!("Clicked 2x{} times", number) }}</Text>
+                <Button onclick={{ *clicks.write() += 1 }}>Click</Button>
+            </Div>
+        }
+    }
+    fn regenerate(&mut self) {
+        when!([self . clicks , self . shown , self . number , self . global , ] => {
+            self . _cached . regenerate ();
+        } else {
+            self . _cached . check_children ();
+        });
+        update!([self . clicks , self . shown , self . number , self . global , ]);
+    }
+    fn get(&self) -> &UiElement {
+        if !self._cached.created() { let _ = self._cached.try_create(|| DynamicUi::new(Box::new(|| self.generate()))); }
+        self._cached.get_element()
     }
 }
